47ee39d1cb0ae26522a13dae48a1391ae68b7aea
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2023-02-25 14:13:07 +0200

    Implementing of non-interactive mode of app
    First, the program reads the data recorded in a text
    file(coefficients separated by spaces) using library "fs".
    After that  the input data is checked for validity. Then,
    if the data passed the test, it is sent to the "quadraticEquation"
    function, which returns the roots of the equation as a string.

---
+++ Lab1/non-interactive.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const fs = require('fs');
+
+const fileContent = fs.readFileSync('equationData.txt', 'utf-8');
+let [a, b, c] = fileContent.split(' ').map(Number);
+
+const quadraticEquation = (a, b, c) => {
+  let Disc = b ** 2 - 4 * a * c;
+
+  if (Disc < 0) return 'There are 0 roots';
+
+  let x1 = (-1 * b + Math.sqrt(Disc)) / (2 * a);
+  let x2 = (-1 * b - Math.sqrt(Disc)) / (2 * a);
+
+  if (Disc === 0) return `There is 1 root\n x1 = ${x1.toFixed(2)}`;
+  else
+    return `There are 2 roots\n x1 = ${x1.toFixed(2)}\n x2 = ${x2.toFixed(2)}`;
+};
+
+if (a === 0) console.log('Error. a cant be 0');
+else if (isNaN(a) || isNaN(b) || isNaN(c)) console.log('Expected real number');
+else {
+  console.log(`Equation is: (${a})x^2 + (${b})x + (${c}) = 0`);
+  console.log(quadraticEquation(a, b, c));
+}
