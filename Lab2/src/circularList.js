71709fadd3b8e9944ee8ed3a0e0c188efc1ce5a3
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2023-03-20 17:20:11 +0200

    reverting error

--- Lab2/src/circularList.js
+++ Lab2/src/circularList.js
@@ -150,7 +150,7 @@ class CircularList {
   }
 
   findFirst(data) {
-    let indOfList = 1;
+    let indOfList = 0;
     let tmp = this.head;
     while (tmp.next !== this.head) {
       if (tmp.data === data) return indOfList;
