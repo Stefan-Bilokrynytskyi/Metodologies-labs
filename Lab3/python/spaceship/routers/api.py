1c452a9f995566f693ff577cde6e0e6043639b46
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2023-04-09 13:02:13 +0300

    Using numpy

--- Lab3/python/spaceship/routers/api.py
+++ Lab3/python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
@@ -6,3 +7,31 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix')
+def matrix() -> dict:
+    firstRows = 5
+    firstColumns = 5
+
+    secondRows = 5
+    secondColumns = 5
+
+    if firstColumns != secondRows:
+
+        response = {
+            'result': 'Error: Sizes of matrix not equal'
+        }
+
+    else:
+
+        firstMatrix = numpy.random.rand(firstRows, firstColumns)
+        secondMatrix = numpy.random.rand(secondRows, secondColumns)
+        result = numpy.dot(firstMatrix, secondMatrix)
+
+        response = {
+            "matrixA": firstMatrix.tolist(),
+            "matrixB": secondMatrix.tolist(),
+            "product": result.tolist()
+        }
+
+    return response
